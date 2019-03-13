22497e60b90f9be0294bfefe93c9283479c6fe5e
Paulo Callebaut <32387119+paulooo13c@users.noreply.github.com>
2019-03-13 16:41:19 +0100

    Update javascript.js
    Grote afbeelding wordt aangepast

--- OefeningenLes/week2/javascript.js
+++ OefeningenLes/week2/javascript.js
@@ -61,7 +61,30 @@
       } else {
         console.log('form contains errors')
       }
+      
+      let thumbs = document.querySelectorAll('.main__thumbs>figure');
+      let big = document.querySelector('.main__large');
+      let photo = big.querySelector('img');
+
+      for (let i = 0; i < thumbs.length; i++) {
+        let thumb = thumbs[i];
+            let link = thumb.querySelector('a');
+            let img = thumb.querySelector('img');
+            let description = document.querySelector('.large__title');
+
+        link.addEventListener('click', function(e){
+          // prevent default link action
+          e.preventDefault();
+          // show image
+          photo.src = link.href;
+          photo.alt = img.alt;
+          description.innerHTML = img.alt;
+          // change active state
+          document.querySelector('.main__thumbs .active').classList.remove('active');
+          thumb.classList.add('active');
+        })
+      }
+      
     });
-    
   });
 })();
