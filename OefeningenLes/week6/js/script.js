840de52bf2db4434b3234ec56ee9b08c604ea9bd
Paulo Callebaut <32387119+paulooo13c@users.noreply.github.com>
2019-04-24 15:33:02 +0200

    nieuw
    commit die ik al een tijde had moeten pushen

---
+++ OefeningenLes/week6/js/script.js
@@ -0,0 +1,38 @@
+;(function(){
+    "use strict";
+
+
+    function handleError(err) {
+	    console.log('Request failed', err)
+	}
+	function handleResponse(resp) {
+    console.log('response status: ', resp.status);
+    // returned the response terug in jsonformaat.
+	  return resp.json();
+  }
+  var kolom1 = document.querySelector('th');
+	function handleSuccess(data) {
+
+      console.log('status: ', data.status);
+      console.log(data.statusText);
+      //kolom1.textContent = data[1 , title];
+      console.log("data1= " ,data[1].title, data[1].nameWithoutThePrefix);  
+      kolom1.innerHTML = data[1].title;
+    }
+  // function showText(responseAsText){
+  //   var title = document.getElementById('songs');
+  //   title.innerHTML = responseAsText;
+  // }
+    
+    document.getElementById("btnSearch").addEventListener("click",function(e){
+        e.preventDefault();
+        let naam = document.getElementById("artist").value;
+        let url = 'https://www.songsterr.com/a/ra/songs/byartists.json?artists='+naam;
+
+        fetch(url)
+        .then(handleResponse)
+        .then(handleSuccess)
+        // .then(showText)
+        .catch(handleError);
+    });
+  }())
