2727044651e23d8914e9c5711e3e72b18cb380e8
Paulo Callebaut <32387119+paulooo13c@users.noreply.github.com>
2019-05-29 15:51:55 +0200

    Werken met classlists
    Overbodige code weggesmeten.
    Werken met classlist eindelijk onder de knie.

--- Project/script.js
+++ Project/script.js
@@ -4,12 +4,7 @@
     window.addEventListener('load', function () {
       console.log("functie 1 werkt");
 
-      // Geprobeerd om op deze manier op elke div te kunnen klikken maar tevergeefs.
-      // let pictogrammen = document.querySelectorAll('dummy');
-      // pictogrammen.forEach(pictogram => pictogram.addEventListener('click', function(){
-      //   console.log("het werkt.");
-      // }));
-
+     
       // Bij het klikken van een div object krijg je de waarde hiervan. En zal er een transformatie plaatsvinden.
       let value1;
       let value2;
@@ -20,38 +15,14 @@
       let kaart = document.getElementsByClassName('klassepictogram');
       let mijnArray = [...kaart];
       for (let i = 0; i < mijnArray.length; i++) {
-        mijnArray[i].addEventListener("click", function(){
-          mijnArray[i].style.transform = "rotatey(180deg)";
-          mijnArray[i].style.backgroundColor = "red";
-          mijnArray[i].classList.remove('klassepictogram');
+          mijnArray[i].addEventListener("click", function(){
           mijnArray[i].classList.add('klik');
           value1 = pictogram.getAttribute('value');
-          VergelijkValue();
+          // VergelijkValue();
           console.log("KlikFunctie werkt.");
           console.log(value1);
         })
       }
-
-
-      // pictogram.addEventListener('click', function(){
-      //   pictogram.style.transform = "rotatey(180deg)";
-      //   //pictogram.style.backgroundColor = "red";
-      //   pictogram.classList.remove('klassepictogram');
-      //   pictogram.classList.add('klik');
-      //   value1 = pictogram.getAttribute('value');
-      //   console.log("KlikFunctie werkt.");
-      //   console.log(value1);
-      // })
-
-      // pictogram2.addEventListener('click', function(){
-      //   pictogram2.style.transform = "rotatey(180deg)";
-      //   pictogram2.style.backgroundColor = "red";
-      //   value2 = pictogram2.getAttribute('value');
-      //   console.log("KlikFunctie 2 werkt.");
-      //   console.log(value2);
-      //   VergelijkValue();
-      // })
-
 
       // Deze functie zal de waarden van beiden vergelijken 
       function VergelijkValue()
