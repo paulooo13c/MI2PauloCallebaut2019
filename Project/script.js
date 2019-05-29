d7f6d3827cfaacf8ccac71ab9a8962354670c3e3
Paulo Callebaut <32387119+paulooo13c@users.noreply.github.com>
2019-05-29 12:21:54 +0200

    Media queries
    Wou met bootstrap werken maar ga dat uiteindelijk niet doen.
    Media queries html 5 toegevoegd

--- Project/script.js
+++ Project/script.js
@@ -16,22 +16,41 @@
       let pictogram = document.getElementById('pictogram');
       let pictogram2 = document.getElementById('pictogram2');
 
-      pictogram.addEventListener('click', function(){
-        pictogram.style.transform = "rotatey(180deg)";
-        pictogram.style.backgroundColor = "red";
-        value1 = pictogram.getAttribute('value');
-        console.log("KlikFunctie werkt.");
-        console.log(value1);
-      })
+      // Klassen in een array steken om ze allemaal te kunnen aanspreken.
+      let kaart = document.getElementsByClassName('klassepictogram');
+      let mijnArray = [...kaart];
+      for (let i = 0; i < mijnArray.length; i++) {
+        mijnArray[i].addEventListener("click", function(){
+          mijnArray[i].style.transform = "rotatey(180deg)";
+          mijnArray[i].style.backgroundColor = "red";
+          mijnArray[i].classList.remove('klassepictogram');
+          mijnArray[i].classList.add('klik');
+          value1 = pictogram.getAttribute('value');
+          VergelijkValue();
+          console.log("KlikFunctie werkt.");
+          console.log(value1);
+        })
+      }
 
-      pictogram2.addEventListener('click', function(){
-        pictogram2.style.transform = "rotatey(180deg)";
-        pictogram2.style.backgroundColor = "red";
-        value2 = pictogram2.getAttribute('value');
-        console.log("KlikFunctie 2 werkt.");
-        console.log(value2);
-        VergelijkValue();
-      })
+
+      // pictogram.addEventListener('click', function(){
+      //   pictogram.style.transform = "rotatey(180deg)";
+      //   //pictogram.style.backgroundColor = "red";
+      //   pictogram.classList.remove('klassepictogram');
+      //   pictogram.classList.add('klik');
+      //   value1 = pictogram.getAttribute('value');
+      //   console.log("KlikFunctie werkt.");
+      //   console.log(value1);
+      // })
+
+      // pictogram2.addEventListener('click', function(){
+      //   pictogram2.style.transform = "rotatey(180deg)";
+      //   pictogram2.style.backgroundColor = "red";
+      //   value2 = pictogram2.getAttribute('value');
+      //   console.log("KlikFunctie 2 werkt.");
+      //   console.log(value2);
+      //   VergelijkValue();
+      // })
 
 
       // Deze functie zal de waarden van beiden vergelijken 
@@ -41,6 +60,7 @@
         if(value1 === value2)
         {
           console.log("de waarden zijn gelijk ")
+
         }
         else
         {
