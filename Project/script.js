3b01dd2b80b9d35f1fd12c83a5a5e616809f41af
Paulo Callebaut <32387119+paulooo13c@users.noreply.github.com>
2019-05-30 17:06:19 +0200

    Begin
    Memory werkt min of meer.
    Nu uitbreidingen

--- Project/script.js
+++ Project/script.js
@@ -1,47 +1,72 @@
 ;;
 (function () {
-    'use strict';
-    window.addEventListener('load', function () {
-      console.log("functie 1 werkt");
+  'use strict';
+  window.addEventListener('load', function () {
+    // Bij het klikken van een div object krijg je de waarde hiervan. En zal er een transformatie plaatsvinden.
 
-     
-      // Bij het klikken van een div object krijg je de waarde hiervan. En zal er een transformatie plaatsvinden.
-      let value1;
-      let value2;
-      let pictogram = document.getElementById('pictogram');
-      let pictogram2 = document.getElementById('pictogram2');
+    // Klassen in een array steken om ze allemaal te kunnen aanspreken.
+    let pictogram = document.getElementsByClassName('klassepictogram');
+    const mijnArray = [...pictogram];
 
-      // Klassen in een array steken om ze allemaal te kunnen aanspreken.
-      let kaart = document.getElementsByClassName('klassepictogram');
-      let mijnArray = [...kaart];
-      for (let i = 0; i < mijnArray.length; i++) {
-          mijnArray[i].addEventListener("click", function(){
-          mijnArray[i].classList.add('klik');
-          value1 = pictogram.getAttribute('value');
-          // VergelijkValue();
-          console.log("KlikFunctie werkt.");
-          console.log(value1);
-        })
-      }
+    let afbeelding = document.getElementsByClassName('hidden');
+    const arrayAfbeeldingen = [...afbeelding];
 
-      // Deze functie zal de waarden van beiden vergelijken 
-      function VergelijkValue()
-      {
-        // Code ....
-        if(value1 === value2)
-        {
-          console.log("de waarden zijn gelijk ")
+    const open = [];
 
+    let isEerstePictogram = false;
+
+    let klik1;
+    let klik2;
+    let afbeelding1;
+    let afbeelding2;
+
+    let geklikedArray = [klik1, klik2, afbeelding1, afbeelding2];
+
+    for (let i = 0; i < mijnArray.length; i++) {
+      mijnArray[i].addEventListener("click", function () {
+        mijnArray[i].classList.add('klik');
+        arrayAfbeeldingen[i].classList.remove('hidden');
+        if (!isEerstePictogram) {
+          isEerstePictogram = true;
+          open.push(mijnArray[i]);
+          open.push(arrayAfbeeldingen[i]);
+          console.log(open[0])
         }
-        else
-        {
-          console.log("de waarden zijn niet gelijk")
-          pictogram.style.transform = "rotatey(180deg)";
-          pictogram.style.backgroundColor = "white";
-          pictogram2.style.transform = "rotatey(180deg)";
-          pictogram2.style.backgroundColor = "white";
+        else {
+          isEerstePictogram = false;
+          open.push(mijnArray[i]);
+          open.push(arrayAfbeeldingen[i]);
+          console.log(open[1]);
+          setTimeout(vergelijken, 500);
+        }
+        console.log("KlikFunctie werkt.");
+      })
+    }
+
+    // Deze functie zal de waarden van beiden vergelijken 
+    let aantalJuist = 0;
+    function vergelijken() {
+      if (open[0].id === open[2].id) {
+        console.log("waarden zijn gelijk");
+        aantalJuist++;
+        open.length = 0;
+        if (aantalJuist == 8) {
+          einde();
         }
       }
-    });
+      else {
+        console.log("waarden zijn niet gelijk")
+        open[0].classList.remove('klik');
+        open[1].classList.add('hidden');
+        open[2].classList.remove('klik')
+        open[3].classList.add('hidden')
+        open.length = 0;
+      }
+    }
+
+    function einde() {
+      alert("Proficiat je hebt ze allemaal gevonden ! ")
+    }
+  });
 
 })();
