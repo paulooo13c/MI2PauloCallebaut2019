7a232c4987d4c69120e65a63fc15eef47bc6d59d
Paulo Callebaut <32387119+paulooo13c@users.noreply.github.com>
2019-05-30 21:14:17 +0200

    laatste tweaks
    Heeft een score teller.
    Highscore bijhouden.
    Code is (hopelijk) wat overzichtelijker.

--- Project/script.js
+++ Project/script.js
@@ -2,8 +2,8 @@
 (function () {
   'use strict';
   window.addEventListener('load', function () {
-    // Bij het klikken van een div object krijg je de waarde hiervan. En zal er een transformatie plaatsvinden.
 
+    // ARRAYS
     // Klassen in een array steken om ze allemaal te kunnen aanspreken.
     let pictogram = document.getElementsByClassName('klassepictogram');
     const mijnArray = [...pictogram];
@@ -14,9 +14,20 @@
     // Een array waar ik de geklikte objecten insteek.
     const open = [];
 
-    // Een boolean die ik gebruik om het eerste en tweede geklikte object te onderscheiden.
-    let isEerstePictogram = false;
+    // DOM elements + variabelen
+    let highscoreBoard = document.getElementById('highscoreBoard');
+    let highscore;
 
+    let aantalJuist = 0;
+    let score = 0;
+    let scoreboard = document.getElementById('scoreboard');
+
+    // Voor het spel te beginnen halen we eerst een highscore binnen als die bestaat.
+    getHighscore();
+
+    // Een boolean die ik gebruik om het eerste en tweede geklikte object te onderscheiden.
+
+    let isEerstePictogram = false;
     for (let i = 0; i < mijnArray.length; i++) {
       mijnArray[i].addEventListener("click", function () {
         mijnArray[i].classList.add('klik');
@@ -32,22 +43,21 @@
           open.push(mijnArray[i]);
           open.push(arrayAfbeeldingen[i]);
           console.log(open[1]);
-          setTimeout(vergelijken, 250);
+          setTimeout(vergelijken, 500);
         }
         console.log("KlikFunctie werkt.");
       })
     }
 
-    // Deze functie zal de waarden van Vergelijken.
-    // In dit geval gaat hij de id van het object er uit nemen.
-
-    let aantalJuist = 0;
+    // Hier gaan we de geplaatse objecten van de array open[] ophalen en de id's vergelijken. Telkens maken we de array leeg zodat de volgende objecten er in gestoken kunnen worden.
 
     function vergelijken() {
       if (open[0].id === open[2].id) {
         console.log("waarden zijn gelijk");
         aantalJuist++;
         open.length = 0;
+        score++
+        scoreboard.textContent = score;
         if (aantalJuist == 8) {
           einde();
         }
@@ -59,18 +69,65 @@
         open[2].classList.remove('klik')
         open[3].classList.add('hidden')
         Buzzer();
+        updateScore();
         open.length = 0;
       }
     }
+
+    // Score updaten
+    function updateScore(){
+      if(score > 0)
+      {
+        score--;
+        scoreboard.textContent = score;
+      }
+      else
+      {
+        ;
+      }
+    }
+
     function Buzzer(){
       var audio = new Audio('sounds/Wrong Buzzer.wav');
       audio.play();
     }
 
     function einde() {
-      alert("Proficiat je hebt ze allemaal gevonden ! ")
-      var credits = new Audio()
+      alert("Proficiat je hebt ze allemaal gevonden!!"
+      +" Het spel word herladen"
+      +" je highscore wordt bijgehouden")
+      aantalJuist = 0;
+      setHighscore();
+      reload();
     }
+
+    function reload(){
+      location.reload();
+    }
+
+    // Als de score die je hebt behaald groter is dan je highscore word deze bijgehouden.
+    function setHighscore(){
+      if(score > localStorage.getItem('highscoreStorage'))
+      {
+        localStorage.setItem('highscoreStorage', score);
+      }
+      else
+      {
+        ;
+      }
+    }
+
+    // Als er een highscore bestaat word deze opgehaald.
+    function getHighscore(){
+      if(localStorage.getItem('highscoreStorage') != null )
+      {
+      highscore = localStorage.getItem('highscoreStorage')
+      highscoreBoard.textContent = highscore;
+      }
+      else{;}
+    
+    }
+  
   });
 
 })();
